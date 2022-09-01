438401d10f300f95f90cba9894602706476d1484
xdadaya <gerakrah@gmail.com>
2022-09-01 20:54:54 +0300

    fix

--- logics/search.js
+++ logics/search.js
@@ -1,63 +1,50 @@
 import Item from "../models/Item.js";
-import Comment from "../models/Comment.js";
-import Collection from "../models/Collection.js";
 import Tag from "../models/Tag.js";
+import Collection from "../models/Collection.js";
+import Comment from "../models/Comment.js";
 
-export const fullTextSearch = async(req, res) => {
-    try{
+export const fullTextSearch = async (req, res) => {
+    try {
         let itemsId = []
-        const itemsFound = await Item.aggregate([
-            { $match: {
-                    $or: [
-                        { 'title': { '$regex': req.params.query, '$options': 'i' } }
-                    ]
-                }}])
-
-        const commentsFound = await Comment.aggregate([
+        const itemsFound = await Item.aggregate([{
+            $search: {
+                index: 'itemIndex',
+                text: {query: req.params.query, path: {wildcard: '*'}}
+            }
+        }])
+        const tags = await Tag.aggregate([{
+            $search: {
+                index: 'tagIndex',
+                text: {query: req.params.query, path: {wildcard: '*'}}
+            }
+        }])
+        const collections = await Collection.aggregate([{
+            $search: {
+                index: 'collectionIndex',
+                text: {query: req.params.query, path: {wildcard: '*'}}
+            }
+        }])
+        const comments = await Comment.aggregate([
             { $match: {
                     $or: [
                         { 'username': { '$regex': req.params.query, '$options': 'i' } },
                         { 'text': { '$regex': req.params.query, '$options': 'i' } }
                     ]
-            }}])
-
-        const collectionsFound = await Collection.aggregate([
-            { $match: {
-                    $or: [
-                        { 'title': { '$regex': req.params.query, '$options': 'i' } },
-                        { 'description': { '$regex': req.params.query, '$options': 'i' } },
-                        { 'theme': { '$regex': req.params.query, '$options': 'i' } },
-                        { 'username': { '$regex': req.params.query, '$options': 'i' } }
-                    ]
                 }}])
 
-        const tagsFound = await Tag.aggregate([
-            { $match: {
-                    $or: [
-                        { 'title': { '$regex': req.params.query, '$options': 'i' } }
-                    ]
-                }}])
 
-        commentsFound.map(({itemId}) => {
+        tags.map(({items})=>{
+            itemsId.push(...items)
+        })
+        collections.map(({items})=>{
+            itemsId.push(...items)
+        })
+        comments.map(({itemId}) => {
             itemsId.push(itemId)
         })
 
-        tagsFound.map(({items}) => {
-            items.map((item)=>{
-                itemsId.push(item)
-            })
-        })
-
-        collectionsFound.map(({items}) => {
-            items.map((item)=>{
-                itemsId.push(item)
-            })
-        })
-
-
-        const foundNotInItem = await Item.find().where('_id').in(itemsId)
-        const allItems = itemsFound.concat(foundNotInItem)
-
+        const notInItem = await Item.find().where('_id').in(itemsId)
+        const allItems = itemsFound.concat(notInItem)
         const items = allItems.filter((value, index) => {
             const _value = JSON.stringify(value);
             return index === allItems.findIndex(obj => {
@@ -65,7 +52,7 @@ export const fullTextSearch = async(req, res) => {
             });
         });
         res.json(items)
-    } catch (e){
+    } catch (e) {
         res.json({message: "Server error searching"})
     }
 }
